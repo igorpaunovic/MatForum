1d2d0f45d583a8c74a517742f10b65b2d6e7620a
Igor Paunovic <igorpaunovic@Igors-MacBook-Pro-2.local>
2025-08-03 23:25:57 +0200

    add reusable generic repository for crud services

--- MatfForum/src/Services/UserManagement/MatForum.UserManagement.Application/Interfaces/IUserRepository.cs
+++ MatfForum/src/Services/UserManagement/MatForum.UserManagement.Application/Interfaces/IUserRepository.cs
@@ -1,13 +1,7 @@
-using MatForum.UserManagement.Application.DTOs;
 using MatForum.UserManagement.Domain.Entities;
+using MatForum.Shared.Domain.Interfaces;
 
 namespace MatForum.UserManagement.Application.Interfaces;
 
-public interface IUserRepository
-{
-    Task<User?> GetById(Guid id);
-    Task<IEnumerable<User>> GetAll();
-    Task<User> Create(User user);
-    Task<User?> Update(Guid id, User user);
-    Task<bool> Delete(Guid id);
-} 
+public interface IUserRepository: IGenericRepository<User>
+{} 
--- MatfForum/src/Services/UserManagement/MatForum.UserManagement.Infrastructure/Repositories/UserRepository.cs
+++ MatfForum/src/Services/UserManagement/MatForum.UserManagement.Infrastructure/Repositories/UserRepository.cs
@@ -1,53 +1,8 @@
 using MatForum.UserManagement.Domain.Entities;
 using MatForum.UserManagement.Application.Interfaces;
+using MatForum.Shared.Infrastructure.Repositories;
 
 namespace MatForum.UserManagement.Infrastructure.Repositories;
 
-public class UserRepository : IUserRepository
-{
-    private static readonly List<User> Users = new();
-    
-    public async Task<User?> GetById(Guid id)
-    {
-        return await Task.FromResult(Users.FirstOrDefault(u => u.Id == id));
-    }
-    
-    public async Task<IEnumerable<User>> GetAll()
-    {
-        return await Task.FromResult(Users.AsEnumerable());
-    }
-    
-    public async Task<User> Create(User user)
-    {
-        if (user.Id == Guid.Empty)
-            user.Id = Guid.NewGuid();
-        
-        user.CreatedAt = DateTime.UtcNow;
-        user.UpdatedAt = DateTime.UtcNow;
-        
-        Users.Add(user);
-        return await Task.FromResult(user);
-    }
-    
-    public async Task<User?> Update(Guid id, User user)
-    {
-        var existingUserIndex = Users.FindIndex(u => u.Id == user.Id);
-        if (existingUserIndex >= 0)
-        {
-            user.UpdatedAt = DateTime.UtcNow;
-            Users[existingUserIndex] = user;
-        }
-        return await Task.FromResult(user);
-    }
-    
-    public async Task<bool> Delete(Guid id)
-    {
-        var user = Users.FirstOrDefault(u => u.Id == id);
-        if (user != null)
-        {
-            Users.Remove(user);
-        }
-        await Task.CompletedTask;
-        return true;
-    }
-}
+public class UserRepository : GenericRepository<User>, IUserRepository
+{}
