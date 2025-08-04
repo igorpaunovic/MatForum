2caa7fc7f834f2e69e78064783def0403368fc73
DjoleXPet <djolepetrovic610@gmail.com>
2025-08-05 01:08:43 +0200

    Implement initial in-memory ForumQuestion service

---
+++ MatfForum/src/Services/ForumQuestion/MatForum.ForumQuestion.Application/DTOs/QuestionDto.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+
+namespace MatForum.ForumQuestion.Application.DTOs
+{
+    public class QuestionDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public Guid CreatedByUserId { get; set; }
+        public string AuthorName { get; set; } // Will be populated from User service (later)
+        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset LastModifiedDate { get; set; }
+        public int Views { get; set; }
+        public bool IsClosed { get; set; }
+        public List<string> Tags { get; set; } = new List<string>();
+    }
+
+    public class CreateQuestionCommand
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public Guid CreatedByUserId { get; set; } // This will come from auth context in real app
+        public List<string> Tags { get; set; } = new List<string>();
+    }
+
+    public class UpdateQuestionCommand
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public List<string> Tags { get; set; } = new List<string>();
+    }
+}
